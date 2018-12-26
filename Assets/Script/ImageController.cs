4f1c6fdcacd212031dd9e669d08f44f372ea7092
watataku <t.watanabe@dcl.cs.waseda.ac.jp>
2018-12-26 17:44:35 +0900

    Line Drawing will be hidden if another marker is detected.

---
+++ Assets/Script/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Events;
+
+// HandTargetを認識した時の動作
+public class ImageController : DefaultTrackableEventHandler
+{
+
+
+    public UnityEvent OnTrackingAction; //マーカーを認識した時の処理
+    public UnityEvent OffTrackingAction;//マーカーが外れた時の処理
+
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        OnTrackingAction.Invoke();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        OffTrackingAction.Invoke();
+    }
+}
--- Assets/Script/TargetManager.cs
+++ Assets/Script/TargetManager.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
 
 public class TargetManager : MonoBehaviour {
 
+    const string imageTargetPath = "/StreamingAssets/Contour/liner_image.png";
+    public GameObject imagePlate;
+
+    public Texture2D TargetImageTexture;
+    public Material TargetImageMaterial;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+        TargetImageTexture = Resources.Load<Texture2D>(Application.dataPath + "Resources/Contour/liner_image.png");
+
+        Debug.Log("LinerImage is");
+        Debug.Log(TargetImageTexture);
+
+        // imagePlate.GetComponent<Renderer>().material.mainTexture = TargetImageTexture;
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 }
